ca3a232ab9b0c2dad8ea14c4fc2b004a6fd5de7c
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-08-05 04:51:51 +0000

    Adding codebase for Heap Insert

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,122 @@
+#include "binary_trees.h"
+
+/**
+  *heap_insert - inserts a value into a Max Binary Heap
+  *@root: double pointer to the root node of the Heap
+  *@value: value stored in the node to be inserted
+  *Return: a pointer to the inserted node, or NULL on failure
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	heap_t *node = NULL;
+	int aux;
+
+    if (root)
+    {
+        if (*root)
+        {
+            node = last_node(*root, value);
+            if (node)
+            {
+                while (node->parent && node->parent->n < node->n)
+                {
+                    aux = node->n;
+                    node->n = node->parent->n;
+                    node->parent->n = aux;
+                    node = node->parent;
+                }
+                return (node);
+            }
+        }
+        else
+        {
+            *root = binary_tree_node(NULL, value);
+		    return (*root);
+        }
+    }
+    return (NULL);
+}
+
+/**
+  *push - a value into the queue
+  *@last: element of queue
+  *@node: to insert
+  *Return: a pointer to the inserted node, or NULL on failure
+*/
+
+queue *push(queue *last, heap_t *node)
+{
+	queue *q = malloc(sizeof(queue));
+
+    if (q)
+    {
+        q->node = node;
+        q->next = NULL;
+        if (last)
+            last->next =  q;
+        return (q);
+    }
+    return (NULL);
+}
+
+/**
+  *last_node - inserts node on the last position of a heap
+  *@root: pointer to the root node of the heap
+  *@value: value stored in the node to be inserted
+  *Return: a pointer to the inserted node, or NULL on failure
+*/
+
+heap_t *last_node(heap_t *root, int value)
+{
+	queue *q, *last, *tmp;
+	heap_t *node = NULL;
+
+	q = push(NULL, root);
+    if (q)
+    {
+        last = q;
+        while (q)
+        {
+            if (node)
+            {
+                tmp = q;
+                q = q->next;
+                free(tmp);
+            }
+            else if (q->node->left && q->node->right)
+            {
+                last = push(last, q->node->left);
+                if (last)
+                {
+                    last = push(last, q->node->right);
+                    if (last)
+                    {
+                        tmp = q;
+                        q = q->next;
+                        free(tmp);
+                    }
+                    else
+                    {
+                        return (NULL);
+                    }
+                }
+                else
+                {
+                    return (NULL);
+                }
+            }
+            else if (!q->node->left)
+            {
+                node = binary_tree_node(q->node, value);
+                q->node->left = node;
+            }
+            else
+            {
+                node = binary_tree_node(q->node, value);
+                q->node->right = node;
+            }
+        }
+        return (node);
+    }
+    return (NULL);
+}
---
+++ 0x02-heap_insert/binary_trees.h
@@ -0,0 +1,45 @@
+
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+typedef struct binary_tree_s heap_t;
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *last_node(heap_t *root, int value);
+
+/**
+* struct queue_s - Queue node.
+* @node: node.
+* @next: Pointer to next node.
+*/
+
+typedef struct queue_s
+{
+	heap_t *node;
+	struct queue_s *next;
+} queue;
+
+queue *push(queue *last, heap_t *node);
+
+#endif /* _BINARY_TREES_H_ */
