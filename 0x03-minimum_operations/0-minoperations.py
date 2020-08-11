d24a66462392f5ae722657057c896cb190ca2c83
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-08-11 08:10:48 +0000

    Add minimal ops Vfinal

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -22,13 +22,12 @@ Number of operations: 6
 
 def minOperations(n):
     opps, text = 0, 'H'
-    if n > 1:
-        while len(text) != n:
-            if n % len(text) == 0:
-                copy = text[:]
-                text += copy
-                opps += 2
-            else:
-                text += copy
-                opps += 1
+    while len(text) != n and n > 1:
+        if n % len(text) == 0:
+            copy = text[:]
+            text += copy
+            opps += 2
+        else:
+            text += copy
+            opps += 1
     return opps
