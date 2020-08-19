c0a7a24520e6eeaaab2cb6a77387a77fe438d251
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-08-19 01:25:06 +0000

    Add codebase for Sandpiles

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,12 +2,6 @@
 #ifndef _SANDPILES_H_
 #define _SANDPILES_H_
 
-// #include <stddef.h>
-// #include <stdlib.h>
-
-// static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-// static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-// int grid[][] grid_sum(int grid1[3][3], int grid2[3][3]);
 
 #endif /* _SANDPILES_H_ */
