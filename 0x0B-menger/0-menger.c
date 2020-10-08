34acc1071d8e05bdfc10e5f20e2a14b55b2b0550
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-10-08 03:33:33 +0000

    Add codebase for Menger

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,34 @@
+#include "menger.h"
+
+/**
+ * menger - draws a 2D Menger Sponge
+ *
+ * @level: the level of the Menger Sponge to draw
+ *
+ */
+
+void menger(int level)
+{
+	char blk;
+	int size, i, j, row, col;
+	
+	size = pow(3, level);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size;)
+		{
+			row = i;
+			col = j++;
+			blk = '#';
+			while (row > 0 || col > 0)
+			{
+				if (row % 3 == 1 && col % 3 == 1)
+					blk = ' ';
+				row = row / 3;
+				col = col / 3;
+			}
+			printf("%c", blk);
+		}
+		printf("\n");
+	}
+}
