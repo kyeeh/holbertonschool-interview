6e49620a9810d44ea572b4a45cce87208bb88855
Ricardo Gutierrez <ricardo@kyeeh.net>
2020-12-03 04:00:23 +0000

    Add codebase for heap sort

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef _HEAP_SORT_H_
+#define _HEAP_SORT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif /* _HEAP_SORT_H_ */
